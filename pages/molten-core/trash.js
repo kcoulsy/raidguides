19f4cac1b017759dba3873037a270c3f671c1a42
Kristian <kristian@coulsy.co.uk>
2019-05-06 12:25:28 +0100

    Fixes

--- pages/molten-core/trash.js
+++ pages/molten-core/trash.js
@@ -10,7 +10,7 @@ const Home = () => (
     <Nav selected="home" />
     <div className="page">
       <BossNav type="mc"/>
-      <div class="content">
+      <div className="content">
         <h2>Trash</h2>
       </div>
     </div>
